e90abea1b1eaec2e3c081306aed5ea1517f5eeed
Donggun.Chung <chungdk1117@gmail.com>
2022-03-17 21:20:41 +0900

    :hammer: 2022.03.17.Thu [converter, formatter]

---
+++ typeconverter/src/test/java/hello/typeconverter/converter/ConversionServiceTest.java
@@ -0,0 +1,24 @@
+package hello.typeconverter.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConversionServiceTest {
+
+    @Test
+    void conversionService() {
+        // enroll
+        DefaultConversionService conversionService = new DefaultConversionService();
+        conversionService.addConverter(new StringToIntegerConverter());
+        conversionService.addConverter(new IntegerToStringConverter());
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        // use
+        assertThat(conversionService.convert("10", Integer.class)).isEqualTo(10);
+        assertThat(conversionService.convert(10, String.class)).isEqualTo("10");
+
+    }
+}
